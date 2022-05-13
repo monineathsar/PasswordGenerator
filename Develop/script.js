c161b98286866cb6d5bde3cbcb8d4dddf5693593
monineathsar <monineathsar@gmail.com>
2022-05-13 13:44:11 -0700

    first draft complete

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // PASSWORD CRITERIA ARRAYS
-var charLength = 8;
+var charLength = 8 - 128;
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l" , "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" , "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
@@ -17,18 +17,23 @@ generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 function writePassword() {
   var criteriaPrompts = selectCriteriaPrompts();
+  var passwordText = document.querySelector("#password");
 
   if (criteriaPrompts){
     var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
     passwordText.value = password;
+  } else {
+    passwordText.value = "";
   }
 }
 // Generates passwords based on selected prompts
 function generatePassword(){
   var password = "";
-  for()
+  for(var i = 0; i < charLength; i++) {
+      var randomChars = Math.floor(Math.random() * passwordCriteria.length);
+      password = password + passwordCriteria[randomChars];
+  }
+  return password;
 }
 // Series of prompts for password criteria once user first clicks on "Generate Password" btn
 function selectCriteriaPrompts(){
@@ -52,7 +57,6 @@ function selectCriteriaPrompts(){
     }
     if (confirm("Does your password require special charecters?")) {
       passwordCriteria = passwordCriteria.concat(specialChar);
-      
     }
     return true;
       
